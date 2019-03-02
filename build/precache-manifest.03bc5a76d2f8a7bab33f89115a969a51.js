baad0ed707f01db91f656cd3c6cfb3e62a451c21
Vineet Kumar <vineet.m95@gmail.com>
2019-03-02 19:35:53 +0530

    Initial Draft

---
+++ build/precache-manifest.03bc5a76d2f8a7bab33f89115a969a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+    {
+        "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+        "url": "/static/media/logo.5d5d9eef.svg"
+    },
+    {
+        "revision": "fdfcfda2d9b1bf31db52",
+        "url": "/static/js/runtime~main.fdfcfda2.js"
+    },
+    {
+        "revision": "ede07932fb63d5b8abfb",
+        "url": "/static/js/main.ede07932.chunk.js"
+    },
+    {
+        "revision": "5bf9b2cda7c63b962686",
+        "url": "/static/js/2.5bf9b2cd.chunk.js"
+    },
+    {
+        "revision": "ede07932fb63d5b8abfb",
+        "url": "/static/css/main.aa18e1d6.chunk.css"
+    },
+    {
+        "revision": "f0ebc544c5ae313134217dd7914d4dbe",
+        "url": "/index.html"
+    }
+];
