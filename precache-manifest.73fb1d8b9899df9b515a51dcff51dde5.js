a2f4df9c678d972061a7fc11541bdbbcf30b10c6
Vineet Mehenwal <vineet.mehenwal@kvantuminc.com>
2019-03-03 16:45:16 +0530

    Updates

---
+++ precache-manifest.73fb1d8b9899df9b515a51dcff51dde5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "f8a76e41b815f7b0438e",
+    "url": "/portfolio/static/js/main.f8a76e41.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/portfolio/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "f8a76e41b815f7b0438e",
+    "url": "/portfolio/static/css/main.282513b6.chunk.css"
+  },
+  {
+    "revision": "f7951ea6648843d70a30b513497bad5a",
+    "url": "/portfolio/index.html"
+  }
+];
